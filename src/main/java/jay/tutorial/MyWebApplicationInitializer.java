e54794f6dbff7f1719bf7f980e7cde9742607c58
wnattawan <wnattawan@gmail.com>
2014-01-07 14:44:30 +0100

    Moved employeeDS to its own config file.

--- src/main/java/jay/tutorial/MyWebApplicationInitializer.java
+++ src/main/java/jay/tutorial/MyWebApplicationInitializer.java
@@ -12,11 +12,13 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext container) throws ServletException {
-		XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation("/WEB-INF/spring/dispatcher-context.xml");
+		XmlWebApplicationContext appContext = new XmlWebApplicationContext();		
+        appContext.setConfigLocation("/WEB-INF/spring/root-context.xml, /WEB-INF/spring/dispatcher-context.xml");
 
         ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet(appContext));
         registration.setLoadOnStartup(1);
         registration.addMapping("/server/*");
     }
+	
+	
 }
